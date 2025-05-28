0b6da221ba6c576067a84e1e58c72f900b11957a
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-05-28 09:09:00 +0300

    feat: add the payment processor

--- src/main/java/com/spring/PaymentProcessor.java
+++ src/main/java/com/spring/PaymentProcessor.java
@@ -1,4 +1,31 @@
 package com.spring;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class PaymentProcessor {
+    private Scanner scanner;
+
+    public PaymentProcessor(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public boolean process() {
+        System.out.println("Méthodes de paiement :");
+        System.out.println("1. Carte de crédit / débit");
+        System.out.println("2. Paiement mobile (Mobile Money)");
+        System.out.print("Choisissez une option (1 ou 2) : ");
+        int choice = scanner.nextInt();
+
+        Random rand = new Random();
+        boolean success = rand.nextBoolean();
+
+        if (!success) {
+            System.out.println("Erreur : Paiement refusé ou problème de connexion.");
+            return false;
+        }
+
+        System.out.println("Paiement accepté.");
+        return true;
+    }
 }
