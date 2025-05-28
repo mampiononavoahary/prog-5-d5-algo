394e43c438e51baa20a67f3bc6f33d5a9e4289c1
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-05-28 09:13:15 +0300

    feat: simulate drink preparation and handle missing cup scenario

--- src/main/java/com/spring/DrinkDispenser.java
+++ src/main/java/com/spring/DrinkDispenser.java
@@ -1,4 +1,27 @@
 package com.spring;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class DrinkDispenser {
+    private Scanner scanner;
+
+    public DrinkDispenser(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void dispense(String drink) {
+        System.out.print("Veuillez placer un gobelet sous le distributeur... (appuyez sur Entrée)");
+        scanner.nextLine();
+        scanner.nextLine();
+
+        Random rand = new Random();
+        if (rand.nextInt(10) < 2) {
+            System.out.println("Erreur : Gobelet manquant. Distribution annulée.");
+            return;
+        }
+
+        System.out.println("Préparation de votre " + drink + "...");
+        System.out.println("Votre " + drink + " est prêt. Bonne dégustation !");
+    }
 }
